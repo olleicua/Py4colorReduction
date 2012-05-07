764c5c3a6c39d0deccad229ce0e6d3605c8d2073
Isaac Dupree <antispam@idupree.com>
2012-05-07 05:28:45 -0400

    Move code that isn't really a doctest at all out of doctests.

--- reductionAttempt.py
+++ reductionAttempt.py
@@ -1177,53 +1177,7 @@ class Configuration :
 	#
 	def findAndShowProofFor(self, name, colorings, smallerConfig = None) :
 		"""
-		>>> smallerConfig = [None]
-
-		>>> config = Configuration([Node("a", 4)], [])
-		>>> config.findAndShowProofFor("Single node of degree 4", config.generatePossibleBoundaryColorings())
-
-		>>> config = Configuration([Node("a", 5)], [])
-		>>> config.findAndShowProofFor("Single node of degree 5", config.generatePossibleBoundaryColorings())
-
-		>>> config = Configuration([Node("a"), Node("b"), Node("c"), Node("d")],
-		...                        [("a", "b"), ("b", "c"), ("c", "d"),
-		...                         ("d", "a"), ("b", "d")])
-		>>> colorings = list(config.generatePossibleBoundaryColoringsTryingCReduction(
-		...                 boxForSmallerConfiguration=smallerConfig))
-		>>> config.findAndShowProofFor("Birkhoff diamond", colorings, smallerConfig[0])
-
-		>>> config = Configuration([Node("a"), Node("b"), Node("c"), Node("d",6), Node("e",6), Node("f",6)],
-		...                       [("a","b"),("a","c"),("a","d"),("a","e"),("a","f"),
-		...                        ("b","c"),("c","d"),("d","e"),("e","f"),("f","b")])
-		>>> config.addBoundary()
-		>>> colorings = list(config.generatePossibleBoundaryColoringsGivenSmallerConfiguration(
-		...                 setsOfBoundaryNodesToMerge=[[config['b#0'],config['b#2'],config['b#4'],config['b#6']]],
-		...                 boxForSmallerConfiguration=smallerConfig))
-		>>> config.findAndShowProofFor("Wilson, exercise 10.3", colorings, smallerConfig[0])
-
-		>>> config = Configuration([Node("a",8), Node("b"), Node("c"), Node("d"), Node("e"), Node("f")],
-		...                       [("a","b"),("a","c"),("a","d"),("a","e"),("a","f"),
-		...                        ("b","c"),("c","d"),("d","e"),("e","f")])
-		>>> config.addBoundary()
-		>>> colorings = config.generatePossibleBoundaryColoringsGivenSmallerConfiguration(
-		...                 setsOfBoundaryNodesToMerge=[[config['b#0'],config['b#7']],
-		...                                             [config['b#1'],config['b#3']],
-		...                                             [config['b#4'],config['b#6']]],
-		...                 boxForSmallerConfiguration=smallerConfig)
-		>>> config.findAndShowProofFor("Wilson, exercise 10.4", colorings, smallerConfig[0])
-
-		TODOs:
-		make a portfolio on my website
-
-		not Returns
-		False, boundaryColoring
-		or
-		True, dictionary* from boundaryColoring to
-		        fullColoring or
-		        (colorPairPair, dictionary from kempeChainSet to (kempeChainsToSwap, fullColoring))
-		*...actually a list of pairs, because boundaryColoring is a dictionary
-		    and dictionaries can't have dictionaries as keys because there's no
-		    frozendict type.
+		This writes some files in './out/'.
 		"""
 		success = []
 		failure = []
@@ -1494,7 +1448,44 @@ class Configuration :
 		results.append("}\n")
 		return ''.join(results)
 
+def showSomeCoolStuff() :
+	smallerConfig = [None]
+
+	config = Configuration([Node("a", 4)], [])
+	config.findAndShowProofFor("Single node of degree 4", config.generatePossibleBoundaryColorings())
+
+	config = Configuration([Node("a", 5)], [])
+	config.findAndShowProofFor("Single node of degree 5", config.generatePossibleBoundaryColorings())
+
+	config = Configuration([Node("a"), Node("b"), Node("c"), Node("d")],
+	                       [("a", "b"), ("b", "c"), ("c", "d"),
+	                        ("d", "a"), ("b", "d")])
+	colorings = list(config.generatePossibleBoundaryColoringsTryingCReduction(
+	                boxForSmallerConfiguration=smallerConfig))
+	config.findAndShowProofFor("Birkhoff diamond", colorings, smallerConfig[0])
+
+	config = Configuration([Node("a"), Node("b"), Node("c"), Node("d",6), Node("e",6), Node("f",6)],
+	                      [("a","b"),("a","c"),("a","d"),("a","e"),("a","f"),
+	                       ("b","c"),("c","d"),("d","e"),("e","f"),("f","b")])
+	config.addBoundary()
+	colorings = list(config.generatePossibleBoundaryColoringsGivenSmallerConfiguration(
+	                setsOfBoundaryNodesToMerge=[[config['b#0'],config['b#2'],config['b#4'],config['b#6']]],
+	                boxForSmallerConfiguration=smallerConfig))
+	config.findAndShowProofFor("Wilson, exercise 10.3", colorings, smallerConfig[0])
+
+	config = Configuration([Node("a",8), Node("b"), Node("c"), Node("d"), Node("e"), Node("f")],
+	                      [("a","b"),("a","c"),("a","d"),("a","e"),("a","f"),
+	                       ("b","c"),("c","d"),("d","e"),("e","f")])
+	config.addBoundary()
+	colorings = config.generatePossibleBoundaryColoringsGivenSmallerConfiguration(
+	                setsOfBoundaryNodesToMerge=[[config['b#0'],config['b#7']],
+	                                            [config['b#1'],config['b#3']],
+	                                            [config['b#4'],config['b#6']]],
+	                boxForSmallerConfiguration=smallerConfig)
+	config.findAndShowProofFor("Wilson, exercise 10.4", colorings, smallerConfig[0])
+
 if __name__ == "__main__" :
 	import doctest
 	doctest.testmod()
+	showSomeCoolStuff()
 
